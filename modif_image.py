245a26fd2cd837dd548f60c987b19d99e4efa304
Thomas Vercier <thomas.vercier@etu.emse.fr>
2024-12-11 21:19:01 +0100

    ferom add

--- modif_image.py
+++ modif_image.py
@@ -53,3 +53,41 @@ def random_mult(img, x, y):
     M = np.random.rand(y_end - y_start, x_end - x_start)
     img[y_start:y_end, x_start:x_end] = img[y_start:y_end, x_start:x_end] * M
     return img
+
+def deform(img,x,y): # https://stackoverflow.com/questions/58237736/how-to-do-deformations-in-opencv
+    """
+    Prend l'image et les coordonnées du clic, et retourne l'image déformée autour du clic.
+
+    Args:
+        img (numpy.ndarray): L'image sur laquelle opérer.
+        x (int): Coordonnée x du clic.
+        y (int): Coordonnée y du clic.
+
+    Returns:
+        numpy.ndarray: L'image modifiée.
+    """
+    radius = 30
+    power = 1.6 # >1.0 for expansion, <1.0 for shrinkage
+    height, width, _ = img.shape
+    map_y = np.zeros((height,width),dtype=np.float32)
+    map_x = np.zeros((height,width),dtype=np.float32)
+    # create index map
+    for i in range(height):
+        for j in range(width):
+            map_y[i][j]=i
+            map_x[i][j]=j  
+    # deform around the right eye
+    for i in range (-radius, radius):
+        for j in range(-radius, radius):
+            if (i**2 + j**2 > radius ** 2):
+                continue
+            if i > 0:
+                map_y[y + i][x + j] = y + (i/radius)**power * radius
+            if i < 0:
+                map_y[y + i][x + j] = y - (-i/radius)**power * radius
+            if j > 0:
+                map_x[y + i][x + j] = x + (j/radius)**power * radius
+            if j < 0:
+                map_x[y + i][x + j] = x - (-j/radius)**power * radius
+    warped=cv.remap(img,map_x,map_y,cv.INTER_LINEAR)
+    return warped
